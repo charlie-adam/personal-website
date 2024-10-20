a24dfdf9e97fed9a11c6bf42df4454b581661cec
charlie-adam <charlietheadam@gmail.com>
2024-10-20 23:32:25 +1000

    Submenu functionality

--- src/app/components/navigation/navigation.component.ts
+++ src/app/components/navigation/navigation.component.ts
@@ -1,4 +1,10 @@
-import { Component, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+import {
+  Component,
+  ViewChild,
+  ElementRef,
+  AfterViewInit,
+  HostListener,
+} from '@angular/core';
 import { CursorFollowDirective } from '../../directives/cursor-follow.directive';
 import { Router, RouterLinkActive, RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
@@ -8,15 +14,75 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [CursorFollowDirective, RouterModule, CommonModule],
   templateUrl: './navigation.component.html',
-  styleUrl: './navigation.component.scss'
+  styleUrl: './navigation.component.scss',
 })
 export class NavigationComponent implements AfterViewInit {
-  @ViewChild('follower', { static: true }) followerRef!: ElementRef;
-  @ViewChild('follower2', { static: true }) followerRef2!: ElementRef;
-  follower!: HTMLElement;
-  follower2!: HTMLElement;
-  ngAfterViewInit() {
-    this.follower = this.followerRef.nativeElement;
-    this.follower2 = this.followerRef2.nativeElement;
+  @ViewChild('moreOptions') moreOptions!: ElementRef;
+
+  lastSelectedDropdown: any;
+  dropdownVisible: boolean = false;
+  ngAfterViewInit() {}
+
+  @HostListener('document:mousemove', ['$event'])
+  onMouseMove(e: MouseEvent) {
+    const target = e.target as HTMLElement;
+    if (target.hasAttribute('moreoptions')) {
+      this.showMoreOptions(target);
+      return;
+    }
+    if (target.parentElement){
+      if (target.classList.contains('option') && target.parentElement.classList.contains('right')) {
+        this.hideMoreOptions();
+        return;
+      }
+    }
+    
+    if (!this.mouseIsNearMenu(e)) {
+      this.hideMoreOptions();
+    }
+  }
+
+  mouseIsNearMenu(e: MouseEvent): boolean {
+    if (!this.moreOptions) return false;
+    const rect = this.moreOptions.nativeElement.getBoundingClientRect();
+    const buffer = 50;
+    const isInsideMenu =
+      e.clientX >= rect.left - buffer &&
+      e.clientX <= rect.right + buffer &&
+      e.clientY >= rect.top - buffer &&
+      e.clientY <= rect.bottom + buffer;
+    return isInsideMenu;
+  }
+
+  showMoreOptions(target: HTMLElement) {
+    if (!this.dropdownVisible || this.lastSelectedDropdown !== target) {
+      this.lastSelectedDropdown = target;
+      this.setDropdownPosition(target);
+      this.toggleDropdown(true);
+    }
+  }
+
+  hideMoreOptions() {
+    this.toggleDropdown(false);
+  }
+
+  toggleDropdown(visible: boolean) {
+    if (this.dropdownVisible !== visible) {
+      this.dropdownVisible = visible;
+      this.moreOptions.nativeElement.style.opacity = visible ? '1' : '0';
+      this.moreOptions.nativeElement.classList.toggle('hidden', !visible);
+    }
+  }
+
+  setDropdownPosition(target: HTMLElement) {
+    const rect = target.getBoundingClientRect();
+    this.moreOptions.nativeElement.style.top = `55px`;
+    this.moreOptions.nativeElement.style.left = `${
+      rect.right - this.moreOptions.nativeElement.offsetWidth
+    }px`;
+  }
+
+  moreOptionsVisible() {
+    return this.moreOptions.nativeElement.style.opacity === '1';
   }
 }
--- src/app/directives/cursor-follow.directive.ts
+++ src/app/directives/cursor-follow.directive.ts
@@ -1,18 +1,76 @@
-import { Directive, HostListener, Input, ElementRef, Renderer2 } from '@angular/core';
+import {
+  Directive,
+  HostListener,
+  Input,
+  ElementRef,
+  Renderer2,
+  OnDestroy,
+} from '@angular/core';
 
 @Directive({
   selector: '[appCursorFollow]',
-  standalone: true
+  standalone: true,
 })
-export class CursorFollowDirective {
-  @Input('appCursorFollow') follower!: HTMLElement;
+export class CursorFollowDirective implements OnDestroy {
+  private followerElement!: HTMLElement;
+  @Input() size = 50;
 
   constructor(private el: ElementRef, private renderer: Renderer2) {}
 
+  randomId() {
+    return Math.random().toString(36).substring(2, 15);
+  }
+  ngOnInit() {
+    const id = this.randomId();
+    this.el.nativeElement.setAttribute('follow-id', id);
+    this.followerElement = this.renderer.createElement('div');
+    this.renderer.addClass(this.followerElement, 'cursor-follow');
+    this.renderer.setStyle(this.followerElement, 'position', 'absolute');
+    this.renderer.setStyle(this.followerElement, 'opacity', '0');
+    this.renderer.setStyle(this.followerElement, 'width', `${this.size}px`);
+    this.renderer.setStyle(
+      this.followerElement,
+      'height',
+      `${this.size * 0.7}px`
+    );
+
+    this.followerElement.setAttribute('follow-target', id);
+
+    this.setFollowerSize();
+    this.renderer.appendChild(document.body, this.followerElement);
+  }
+
+  setFollowerSize() {
+    const parentRect = this.el.nativeElement.getBoundingClientRect();
+    const parentWidth = parentRect.width;
+
+    const followerSize = parentWidth < 50 ? parentWidth * 0.2 : this.size;
+    this.renderer.setStyle(this.followerElement, 'width', `${followerSize}px`);
+    this.renderer.setStyle(
+      this.followerElement,
+      'height',
+      `${followerSize * 0.7}px`
+    );
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(e: Event) {
+    this.setFollowerSize();
+  }
+
+  @HostListener('document:mousemove', ['$event'])
   @HostListener('document:mousemove', ['$event'])
   onMouseMove(e: MouseEvent) {
+    const isParentHidden =
+      this.el.nativeElement.style.opacity === '0' ||
+      this.el.nativeElement.classList.contains('hidden');
+    if (isParentHidden) {
+      this.renderer.setStyle(this.followerElement, 'opacity', '0');
+      return;
+    }
+    const parentStyle = window.getComputedStyle(this.el.nativeElement);
     const rect = this.el.nativeElement.getBoundingClientRect();
-    const followerRect = this.follower.getBoundingClientRect();
+    const followerRect = this.followerElement.getBoundingClientRect();
     const followerWidth = followerRect.width;
     const followerHeight = followerRect.height;
 
@@ -39,12 +97,17 @@ export class CursorFollowDirective {
       } else if (desiredY + followerHeight / 2 > rect.bottom) {
         constrainedY = rect.bottom - followerHeight / 2;
       }
-
-      this.renderer.setStyle(this.follower, 'opacity', '1');
-      this.renderer.setStyle(this.follower, 'left', `${constrainedX}px`);
-      this.renderer.setStyle(this.follower, 'top', `${constrainedY}px`);
+      this.renderer.setStyle(this.followerElement, 'opacity', '1');
+      this.renderer.setStyle(this.followerElement, 'left', `${constrainedX}px`);
+      this.renderer.setStyle(this.followerElement, 'top', `${constrainedY}px`);
     } else {
-      this.renderer.setStyle(this.follower, 'opacity', '0');
+      this.renderer.setStyle(this.followerElement, 'opacity', '0');
+    }
+  }
+
+  ngOnDestroy() {
+    if (this.followerElement) {
+      this.renderer.removeChild(document.body, this.followerElement);
     }
   }
 }
